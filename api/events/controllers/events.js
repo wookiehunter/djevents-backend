3096c5643bc443a3054aee93064c3b8ce9126ea7
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-05-28 08:57:41 +0100

    events crud context

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -8,6 +8,64 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 
 module.exports = {
+  // create event to link user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  // event update
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.events.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.events.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+  // Delete a user event
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    const entity = await strapi.services.events.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  // get logged in user
   async me(ctx) {
     const user = ctx.state.user;
 
@@ -17,12 +75,12 @@ module.exports = {
       ]);
     }
 
-    const data = await strapi.services.events.find({user: user.id})
+    const data = await strapi.services.events.find({ user: user.id });
 
     if (!data) {
-        return ctx.notFound()
+      return ctx.notFound();
     }
 
-    return sanitizeEntity(data, {model: strapi.models.events})
+    return sanitizeEntity(data, { model: strapi.models.events });
   },
 };
